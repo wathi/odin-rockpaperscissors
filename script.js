747d65d04bcbabbd6507802f751729f985e5da3b
HW <68807000+wathi@users.noreply.github.com>
2023-10-08 13:02:30 +0100

    Display Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3);
+    let radNum = Math.floor(Math.random() * 3);
+    console.log(radNum);
+    if (radNum == 0) {
+        return "Rock";
+    } else if (radNum == 1){
+        return "Paper";
+    } else if (radNum == 2){
+        return "Scissors";
+    }
 }
 
 console.log(getComputerChoice());
